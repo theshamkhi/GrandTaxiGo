6e96a90abe7ffacf3a910d3d9311c0d8bd65f562
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-02-26 12:22:17 +0000

    Added fillable fileds

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'profile_photo',
+        'photo',
         'role',
         'phone',
         'location',
